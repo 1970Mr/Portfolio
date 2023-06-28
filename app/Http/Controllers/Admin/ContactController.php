3b317ea3adae2be896c2508620d7f365aa509487
1970Mr <mrepoch1970@gmail.com>
2023-06-28 13:07:59 +0430

    Add Contact (details) routes

--- app/Http/Controllers/Admin/ContactController.php
+++ app/Http/Controllers/Admin/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\ContactRequest;
 use App\Models\Contact;
 
 class ContactController extends Controller
