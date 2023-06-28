3e99b4a6c3ed98b992f2fdbdc4345aa6af1258b6
1970Mr <mrepoch1970@gmail.com>
2023-06-28 12:51:35 +0430

    Update Contact controller

--- app/Http/Controllers/Admin/ContactController.php
+++ app/Http/Controllers/Admin/ContactController.php
@@ -3,83 +3,47 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Contact;
 
 class ContactController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        $contacts = Contact::paginate(5);
+		return view('admin.contact.contact', compact('contacts'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        return view('admin.contact.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function store(ContactRequest $request)
     {
-        //
+        $request['status'] = $request->has('status');
+
+        Contact::create($request->all());
+
+		return to_route('admin.panel.about.contact')->with(['success' => 'عملیات ایجاد با موفقیت انجام شد']);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function edit(Contact $contact)
     {
-        //
+        return view('admin.contact.edit', compact('contact'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function update(ContactRequest $request, Contact $contact)
     {
-        //
+        $request['status'] = $request->has('status');
+        $contact->updateOrFail($request->all());
+
+		return to_route('admin.panel.about.contact')->with(['success' => 'عملیات ویرایش با موفقیت انجام شد']);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function destroy(Contact $contact)
     {
-        //
-    }
+        $contact->delete();
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+		return redirect()->back()->with(['success' => 'عملیات حذف با موفقیت انجام شد']);
     }
 }
