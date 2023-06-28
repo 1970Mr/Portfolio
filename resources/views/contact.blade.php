c3b6fbf91f829e5668b34acc6d9768a4f2fca2c5
github-1970 <github1970@gmail.com>
2023-06-28 18:18:56 +0430

    Add view in user interface for Contact Me. and fixed Some bugs.

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,74 +1,100 @@
 @extends('layouts.app', ['title' => 'ارتباط با من', 'my_class' => 'contact'])
 
 @section('content')
-
-<!-- Page Title Starts -->
-<section class="title-section text-left text-sm-center revealator-slideup revealator-once revealator-delay1">
+  <!-- Page Title Starts -->
+  <section class="title-section text-left text-sm-center revealator-slideup revealator-once revealator-delay1">
     <h1>ارتباط <span>با من</span></h1>
     <span class="title-bg">ارتباط</span>
-</section>
-<!-- Page Title Ends -->
-<!-- Main Content Starts -->
-<section class="main-content revealator-slideup revealator-once revealator-delay1">
+  </section>
+  <!-- Page Title Ends -->
+  <!-- Main Content Starts -->
+  <section class="main-content revealator-slideup revealator-once revealator-delay1">
     <div class="container">
-        <div class="row">
-            <!-- Left Side Starts -->
-            <div class="col-12 col-lg-4">
-                <h3 class="text-uppercase custom-title mb-0 ft-wt-600 pb-3">خجالت نکشید!</h3>
-                <p class="open-sans-font mb-3">در صورت تمایل با من در تماس باشید. من همیشه در مورد بحث درباره پروژه های جدید ، ایده های خلاقانه یا فرصت هایی که بخشی از دیدگاه های شما باشد ، باز هستم.</p>
-                <p class="open-sans-font custom-span-contact position-relative">
-                    <i class="fa fa-envelope-open position-absolute"></i>
-                    <span class="d-block">ایمیل من</span>steve@mail.com
-                </p>
-                <p class="open-sans-font custom-span-contact position-relative">
-                    <i class="fa fa-phone-square position-absolute"></i>
-                    <span class="d-block">تلفن من</span>09013234324
-                </p>
-                <ul class="social list-unstyled pt-1 mb-5">
-                    <li class="facebook"><a title="Facebook" href="#"><i class="fa fa-facebook"></i></a>
-                    </li>
-                    <li class="twitter"><a title="Twitter" href="#"><i class="fa fa-twitter"></i></a>
-                    </li>
-                    <li class="youtube"><a title="Youtube" href="#"><i class="fa fa-youtube"></i></a>
-                    </li>
-                    <li class="dribbble"><a title="Dribbble" href="#"><i class="fa fa-dribbble"></i></a>
-                    </li>
-                </ul>
-            </div>
-            <!-- Left Side Ends -->
-            <!-- Contact Form Starts -->
-            <div class="col-12 col-lg-8">
-                <form class="contactform" method="post" action="https://tunis.setinco.com/dark/php/process-form.php">
-                    <div class="contactform">
-                        <div class="row">
-                            <div class="col-12 col-md-4">
-                                <input type="text" name="name" placeholder="نام شما ">
-                            </div>
-                            <div class="col-12 col-md-4">
-                                <input type="email" name="email" placeholder="ایمیل شما ">
-                            </div>
-                            <div class="col-12 col-md-4">
-                                <input type="text" name="subject" placeholder="موضوع شما ">
-                            </div>
-                            <div class="col-12">
-                                <textarea name="message" placeholder="پیغام شما"></textarea>
-                                <button type="submit" class="button">
-                                    <span class="button-text">ارسال پیام</span>
-                                    <span class="button-icon fa fa-send"></span>
-                                </button>
-                            </div>
-                            <div class="col-12 form-message">
-                                <span class="output_message text-center font-weight-600 text-uppercase"></span>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <!-- Contact Form Ends -->
+      <div class="row">
+        <!-- Left Side Starts -->
+        <div class="col-12 col-lg-4">
+          <h3 class="text-uppercase custom-title mb-0 ft-wt-600 pb-3">{{ $contact->title }}</h3>
+          <p class="open-sans-font mb-3 description">{{ $contact->description }}</p>
+          <p class="open-sans-font custom-span-contact position-relative">
+            <i class="fa fa-envelope-open position-absolute"></i>
+            <span class="d-block">ایمیل من</span>
+            <a class="text-white" href="mailto:{{ $contact->email }}">{{ $contact->email }}</a>
+          </p>
+          <p class="open-sans-font custom-span-contact position-relative">
+            <i class="fa fa-phone-square position-absolute"></i>
+            <span class="d-block">تلفن من</span>
+            <a class="text-white" href="tel:{{ $contact->phone_number }}" dir="ltr">{{ $contact->phone_number }}</a>
+          </p>
+          <ul class="social list-unstyled pt-1 mb-5 d-flex">
+
+            @foreach ($socials as $social)
+              @if ($contact->{$social['name']})
+                <li>
+                  <a target="_blank" title="{{ $social['name'] }}" href="{{ $social['link'] . '/' . $contact->{$social['name']} }}">
+                    <i class="fa fa-{{ $social['name'] }}"></i>
+                  </a>
+                </li>
+              @endif
+            @endforeach
+            {{-- <li class="telegram"><a title="Telegram" href="#"><i class="fa fa-telegram"></i></a></li>
+            <li class="instagram"><a title="Instagram" href="#"><i class="fa fa-instagram"></i></a></li>
+            <li class="twitter"><a title="Twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+            <li class="youtube"><a title="Youtube" href="#"><i class="fa fa-youtube"></i></a></li>
+            <li class="facebook"><a title="Facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+            <li class="linkedin"><a title="Linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+            <li class="github"><a title="Github" href="#"><i class="fa fa-github"></i></a></li> --}}
+          </ul>
         </div>
+        <!-- Left Side Ends -->
+        <!-- Contact Form Starts -->
+        <div class="col-12 col-lg-8">
+          <form class="contactform" method="post" action="https://tunis.setinco.com/dark/php/process-form.php">
+            <div class="contactform">
+              <div class="row">
+                <div class="col-12 col-md-4">
+                  <input type="text" name="name" placeholder="نام شما ">
+                </div>
+                <div class="col-12 col-md-4">
+                  <input type="email" name="email" placeholder="ایمیل شما ">
+                </div>
+                <div class="col-12 col-md-4">
+                  <input type="text" name="subject" placeholder="موضوع شما ">
+                </div>
+                <div class="col-12">
+                  <textarea name="message" placeholder="پیغام شما"></textarea>
+                  <button type="submit" class="button">
+                    <span class="button-text">ارسال پیام</span>
+                    <span class="button-icon fa fa-send"></span>
+                  </button>
+                </div>
+                <div class="col-12 form-message">
+                  <span class="output_message text-center font-weight-600 text-uppercase"></span>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+        <!-- Contact Form Ends -->
+      </div>
     </div>
 
-</section>
-<!-- Main Content Ends -->
-
+  </section>
+  <!-- Main Content Ends -->
 @endsection
+
+@push('styles')
+  <style>
+    p.description {
+      white-space: pre-wrap;
+      line-height: 1.6rem;
+    }
+
+    form .row {
+      padding-top: 2.6rem;
+    }
+
+    textarea {
+      height: 200px !important;
+    }
+  </style>
+@endpush
