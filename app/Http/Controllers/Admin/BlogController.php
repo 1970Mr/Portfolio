1bdccca4ad5a1140aaa2bccfffd3f5418fe6d10b
1970Mr <mrepoch1970@gmail.com>
2023-06-29 23:01:38 +0430

    Add store method for Blog Controller

--- app/Http/Controllers/Admin/BlogController.php
+++ app/Http/Controllers/Admin/BlogController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\BlogRequest;
 use App\Models\Blog;
 
 class BlogController extends Controller
@@ -16,5 +17,21 @@ class BlogController extends Controller
     public function create()
     {
         return view('admin.blog.create');
+    }
+
+    public function store(BlogRequest $request)
+    {
+        $request['status'] = $request->has('status');
+
+        $request['photo'] = $this->imageUpload($request);
+
+        Blog::create($request->all());
+
+		return to_route('admin.panel.blogs')->with(['success' => 'عملیات ایجاد با موفقیت انجام شد']);
+    }
+
+    private function imageUpload($request)
+    {
+        return image_upload($request->file('photo'), public_path("images/blog"));
     }
 }
