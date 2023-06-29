67337604b0a3b280f84bf43437bb3c5cd03a09a8
github-1970 <github1970@gmail.com>
2023-06-29 22:37:55 +0430

    Add Model for Blog

---
+++ app/Models/Blog.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'text',
+        'author',
+        'keywords',
+        'photo',
+        'status',
+    ];
+
+    protected function photo(): Attribute
+	{
+		return Attribute::make(
+			get: fn ($value) => json_decode($value, true),
+			set: fn ($value) => json_encode($value),
+		);
+	}
+}
