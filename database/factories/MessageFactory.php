e91e13b4f987d6e37d6abbf52efdf688a7291cb8
1970Mr <mrepoch1970@gmail.com>
2023-06-30 23:19:59 +0430

    Add factory for Message

---
+++ database/factories/MessageFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Message>
+ */
+class MessageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->email(),
+            'subject' => fake()->text(10),
+            'message' => fake()->text(70),
+            'response' => fake()->name(50),
+        ];
+    }
+}
