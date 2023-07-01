f63621700b759d415f9d0c4bd4b8761ade2dedab
1970Mr <mrepoch1970@gmail.com>
2023-07-01 13:39:17 +0430

    Add index method for MessageController

---
+++ app/Http/Controllers/Admin/MessageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Message;
+use Illuminate\Http\Request;
+
+class MessageController extends Controller
+{
+    public function index()
+    {
+        $messages = Message::paginate(5);
+		return view('admin.message.message', compact('messages'));
+    }
+}
