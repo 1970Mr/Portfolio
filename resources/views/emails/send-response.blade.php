314173b32287a239319539fd5a4e3a560bd0e7b1
1970Mr <mrepoch1970@gmail.com>
2023-07-01 18:00:37 +0430

    Add email view for sendResponse in MessageController

---
+++ resources/views/emails/send-response.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html dir="rtl">
+<head>
+    <meta charset="utf-8">
+    <title>{{ $subject }}</title>
+</head>
+<body>
+    <h2>{{ $greeting }}</h2>
+
+    <p>{{ $intro }}</p>
+
+    <p>{{ $content }}</p>
+
+    <p>{{ $farewell }}</p>
+
+    <p>{{ $signature }}</p>
+</body>
+</html>
