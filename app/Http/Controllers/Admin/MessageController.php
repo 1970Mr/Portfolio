f3ae224975912dfa86ba49614352c96c9f372bf2
1970Mr <mrepoch1970@gmail.com>
2023-07-01 15:05:30 +0430

    Add store method for MessageController

--- app/Http/Controllers/Admin/MessageController.php
+++ app/Http/Controllers/Admin/MessageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\MessageRequest;
 use App\Models\Message;
 use Illuminate\Http\Request;
 
@@ -12,5 +13,12 @@ class MessageController extends Controller
     {
         $messages = Message::paginate(5);
 		return view('admin.message.message', compact('messages'));
+    }
+
+    public function store(MessageRequest $request)
+    {
+        $data = $request->except(['response', 'is_read']);
+        Message::create($data);
+		return to_route('admin.panel.contact.message')->with(['success' => 'پیام شما با موفقیت ارسال شد! بزودی به پیام شما پاسخ داده خواهد شد.']);
     }
 }
--- app/Http/Requests/Admin/MessageRequest.php
+++ app/Http/Requests/Admin/MessageRequest.php
@@ -25,7 +25,7 @@ class MessageRequest extends FormRequest
     {
         return [
             'name' => 'required',
-            'email' => 'required',
+            'email' => 'required|email',
             'subject' => 'required',
             'message' => 'required',
         ];
