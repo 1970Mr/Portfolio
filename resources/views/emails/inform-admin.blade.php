cbcfe5b40aede643dc04607ac574008990debf73
1970Mr <mrepoch1970@gmail.com>
2023-07-03 23:25:34 +0430

    Add email template to inform admin has message

---
+++ resources/views/emails/inform-admin.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html dir="rtl">
+<head>
+    <meta charset="utf-8">
+    <title>{{ $subject }}</title>
+</head>
+<body>
+    <h2>{{ $greeting }}</h2>
+
+    <p style="white-space: pre-wrap;">{{ $content }}</p>
+</body>
+</html>
