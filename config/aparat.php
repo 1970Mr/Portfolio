70793470ef6c899aba88516042d34c9ae862e8e9
1970Mr <mrepoch1970@gmail.com>
2023-07-05 20:53:55 +0430

    Add the video delete from aparat in the Aparat service and Portfolio Controller

--- config/aparat.php
+++ config/aparat.php
@@ -4,6 +4,7 @@ return [
     'login' => 'https://www.aparat.com/etc/api/login/luser/{user}/lpass/{pass}',
     'upload_​form' => 'https://www.aparat.com/etc/api/upload​form/luser/{user}/ltoken/{token}',
     'video' => 'https://www.aparat.com/etc/api/video/videohash/{uid}',
+    'deletevideolink' => 'https://www.aparat.com/etc/api/deletevideolink/videohash/{uid}/luser/{user}/ltoken/{token}',
     'tech_category_id' => 10,
     'username' => env('APARAT_USERNAME', ''),
     'password' => env('APARAT_PASSWORD', ''),
