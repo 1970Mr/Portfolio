7169a382a12d8618db5edf6cec0d73c49081025b
1970Mr <mrepoch1970@gmail.com>
2023-07-05 15:54:58 +0430

    Add getToken and replaceRequirement for Aparat Service

--- config/aparat.php
+++ config/aparat.php
@@ -4,4 +4,6 @@ return [
     'login' => 'https://www.aparat.com/etc/api/login/luser/{user}/lpass/{pass}',
     'upload_​form' => 'https://www.aparat.com/etc/api/upload​form/luser/{user}/ltoken/{token}',
     'tech_category_id' => 10,
+    'username' => env('APARAT_USERNAME', ''),
+    'password' => env('APARAT_PASSWORD', ''),
 ];
