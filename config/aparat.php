24d53776d14568fdd94ae5725ff3233858e81848
github-1970 <github1970@gmail.com>
2023-07-05 14:41:26 +0430

    Add Aparat config

---
+++ config/aparat.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'login' => 'https://www.aparat.com/etc/api/profile/username/{user}',
+    'upload_​form' => 'https://www.aparat.com/etc/api/upload​form/luser/{user}/ltoken/{token}',
+    'tech_category_id' => 10,
+];
