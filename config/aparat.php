b4e47acc93acad64b3b9892f0bcc095f3184ab83
github-1970 <github1970@gmail.com>
2023-07-05 18:43:13 +0430

    Increase max_execution_time for upload file in Aparat Service

--- config/aparat.php
+++ config/aparat.php
@@ -3,7 +3,7 @@
 return [
     'login' => 'https://www.aparat.com/etc/api/login/luser/{user}/lpass/{pass}',
     'upload_​form' => 'https://www.aparat.com/etc/api/upload​form/luser/{user}/ltoken/{token}',
-    'video_info' => 'https://www.aparat.com/etc/api/video/videohash/{uid}',
+    'video' => 'https://www.aparat.com/etc/api/video/videohash/{uid}',
     'tech_category_id' => 10,
     'username' => env('APARAT_USERNAME', ''),
     'password' => env('APARAT_PASSWORD', ''),
