cb9a301703eb9a7151a5b5d1376c4e4dab78d667
1970Mr <mrepoch1970@gmail.com>
2023-07-09 19:16:21 +0330

    Add index on Portfolio Controller

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Portfolio;
 use Illuminate\Http\Request;
 
 class PortfolioController extends Controller
 {
-    //
+    public function index()
+    {
+        $portfolios = Portfolio::all();
+        return view('portfolio', compact('portfolios'));
+    }
 }
