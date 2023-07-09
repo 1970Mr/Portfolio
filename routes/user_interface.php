6585ef049b642d082d100aef8d71d181a1552eb8
1970Mr <mrepoch1970@gmail.com>
2023-07-09 19:24:28 +0330

    Add index route Portfolio

--- routes/user_interface.php
+++ routes/user_interface.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AboutController;
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PortfolioController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -11,14 +12,12 @@ Route::get('/home', [HomeController::class, 'index']);
 
 Route::get('/about', [AboutController::class, 'index'])->name('about');
 
-Route::get('/portfolio', function () {
-  return view('portfolio');
-})->name('portfolio');
-
 Route::get('/contact', [ContactController::class, 'index'])->name('contact');
 
 Route::get('/blogs', [BlogController::class, 'index'])->name('blog');
 Route::get('/blogs/{blog}', [BlogController::class, 'show'])->name('blog.show');
+
+Route::get('/portfolio', [PortfolioController::class, 'index'])->name('portfolio');
 
 Route::get('/blog-post', function () {
   return view('blog-post');
