4d535909235c35f379e8c6e55b205cc5a5bb3316
1970Mr <mrepoch1970@gmail.com>
2023-07-10 17:55:51 +0330

    Add font for login page

--- resources/views/auth/layouts/app.blade.php
+++ resources/views/auth/layouts/app.blade.php
@@ -9,6 +9,23 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.rtl.min.css"
     integrity="sha384-beJoAY4VI2Q+5IPXjI207/ntOuaz06QYCdpWfWRv4lSFDyUSqsM0W+wiAMr2I185" crossorigin="anonymous">
 
+  <style>
+    @font-face {
+      font-family: "Vazir";
+      src: url("{{ asset('admin/fonts/Vazir.eot') }}");
+      /* IE9 Compat Modes */
+      src: url("{{ asset('admin/fonts/Vazir.eot?#iefix') }}") format("embedded-opentype"),
+        url("{{ asset('admin/fonts/Vazir.woff2') }}") format("woff2"),
+        url("{{ asset('admin/fonts/Vazir.woff') }}") format("woff"),
+        url("{{ asset('admin/fonts/Vazir.ttf') }}") format("truetype");
+      /* Safari, Android, iOS */
+    }
+
+    body {
+      font-family: "Vazir" !important;
+    }
+  </style>
+
   @stack('styles')
 
   <title>{{ env('APP_NAME') . ' | لاگین' . (isset($title) && !empty($title) ? " | $title" : '') }}</title>
