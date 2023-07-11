23c486bfb4d57e5a3797f414e5838e6ee25e52ad
1970Mr <mrepoch1970@gmail.com>
2023-07-11 04:05:03 +0330

    Update profile view for add change password

--- resources/views/admin/blog.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('admin.layouts.app', ['title' => 'مقالات'])
-
-@section('content')
-  blog
-@endsection
--- resources/views/admin/portfolio.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('admin.layouts.app', ['title' => 'نمونه کار'])
-
-@section('content')
-  portfolio
-@endsection
