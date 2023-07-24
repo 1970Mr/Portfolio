4d19a43f487941e5b02ae788c5c2bd650dd5e4de
github-1970 <github1970@gmail.com>
2023-07-24 22:49:35 +0330

    Refactor home routes

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,9 @@
 <?php
 
-include 'user_interface.php';
-include 'admin.php';
+use Illuminate\Support\Facades\Route;
+
+require_once('user_interface.php');
+
+Route::prefix('admin-panel')->middleware('auth')->name('admin.panel.')->group(function () {
+    require_once('admin.php');
+});
