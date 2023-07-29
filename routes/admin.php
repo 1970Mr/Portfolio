d4d2e2381e70e98d2b70f0fa0bec1fff8c8da73f
1970Mr <mrepoch1970@gmail.com>
2023-07-29 19:49:08 +0330

    Refactor portfolio routes

--- routes/admin.php
+++ routes/admin.php
@@ -41,14 +41,7 @@ Route::prefix('about')->name('about.')->group(function () {
 });
 
 Route::resource('portfolios', PortfolioController::class)->except('show');
-Route::delete('/portfolios/media/{portfolio}', [PortfolioController::class, 'destroyMedia'])->name('portfolio.destroy.media');
-
-// Route::get('/portfolio', [PortfolioController::class, 'index'])->name('portfolio');
-// Route::get('/portfolio/create', [PortfolioController::class, 'create'])->name('portfolio.create');
-// Route::post('/portfolio', [PortfolioController::class, 'store'])->name('portfolio.store');
-// Route::get('/portfolio/edit/{portfolio}', [PortfolioController::class, 'edit'])->name('portfolio.edit');
-// Route::put('/portfolio/{portfolio}', [PortfolioController::class, 'update'])->name('portfolio.update');
-// Route::delete('/portfolio/{portfolio}', [PortfolioController::class, 'destroy'])->name('portfolio.destroy');
+Route::delete('/portfolios/media/{portfolio}', [PortfolioController::class, 'destroyMedia'])->name('portfolios.destroy.media');
 
 Route::prefix('contact')->name('contact.')->group(function () {
     Route::get('/details', [ContactController::class, 'index'])->name('details');
