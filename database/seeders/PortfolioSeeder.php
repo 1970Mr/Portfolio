8162ed13e74f50a6df63b0d4a6eaba8f7d9acbc1
1970Mr <mrepoch1970@gmail.com>
2024-02-13 20:50:01 +0330

    PortfolioSeeder refactoring

--- database/seeders/PortfolioSeeder.php
+++ database/seeders/PortfolioSeeder.php
@@ -15,37 +15,9 @@ class PortfolioSeeder extends Seeder
      */
     public function run()
     {
-        Portfolio::factory()->createMany([
-            [
-                'title' => 'سایت portfolio',
-                'project_type' => 'طراحی و توسعه سایت',
-                'customer' => 'خودم',
-                'link' => 'rasmor.ir',
-                'technology' => 'php, laravel',
-                'featured_image' => [
-                    'name' => 'portfolio.jpg',
-                    'relative_path' => 'seeder/images/portfolio/portfolio.jpg'
-                ],
-                'media_type' => Portfolio::$mediaTypes[0],
-                'media' => null,
-                'status' => true,
-            ],
-            [
-                'title' => 'سایت خبری mohebnews',
-                'project_type' => 'طراحی سایت',
-                'customer' => 'یکی از آشنایان',
-                'link' => 'mohebnews.com',
-                'technology' => 'wordpress',
-                'featured_image' => [
-                    'name' => 'portfolio2.jpg',
-                    'relative_path' => 'seeder/images/portfolio/portfolio2.jpg'
-                ],
-                'media_type' => Portfolio::$mediaTypes[0],
-                'media' => null,
-                'status' => true,
-            ]
-        ]);
-
+        $json_data = file_get_contents('database/seeders/json_data/portfolio.json');
+        $portfolios = json_decode($json_data, true);
+        Portfolio::factory()->createMany($portfolios);
         Portfolio::factory(10)->state(['status' => false])->create();
     }
 }
